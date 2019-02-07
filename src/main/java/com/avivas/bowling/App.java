2ab4e81679fdc36c3f583a0e59ab62ecd367037d
Alexander Vivas <falexvr@gmail.com>
2019-02-06 20:31:14 -0500

    First setup

---
+++ src/main/java/com/avivas/bowling/App.java
@@ -0,0 +1,13 @@
+package com.avivas.bowling;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
